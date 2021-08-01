941dcc6cd625edc37bedfebad623159c7cf4c004
Danilo <danilo.guimaraes@outlook.com.br>
2021-07-31 22:26:49 -0400

    Tests for Users table.

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,10 +1,38 @@
 const { Section1 } = require('../objects/section-1')
 
-describe('Problem 1', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section1, you can do: Section1.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+before(() => {
+  Section1.actions.visit()
+})
+
+describe('Validate the Users table.', () => {  
+  context('user table visibility', () => {
+    it('is hidden when page loads', () => {
+      Section1.actions.assertUsersTableIsHidden()  
+    })
+
+    it('is visible when clicking on show button', () => {
+      Section1.actions.clickShowTable()
+      Section1.actions.assertUsersTableIsVisible()
+    })
+  })
+
+  context('user table structure', () => {
+    it('is 5 columns wide', () => {
+      Section1.actions.assertUsersTableColumnCount(5)
+    })
+
+    it('is 10 rows long', () => {
+      Section1.actions.assertUsersTableRowCount(10)
+    })
+  })
+
+  context('user table data', () => {
+    it('has at least 5 entries have the role "user"', () => {
+      Section1.actions.assertUserCountWithRole('user', 5, false)
+    })
+
+    it('has exactly 3 people older than 60 years old', () => {
+      Section1.actions.assertUserCountOlderThanAge(60, 3)
+    })
+  })
 })
