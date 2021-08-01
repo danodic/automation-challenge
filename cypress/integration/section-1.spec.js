7b8fb7e13952c30a1fba90cb28aa111cd715c924
Danilo <danilo.guimaraes@outlook.com.br>
2021-08-01 16:08:34 -0400

    Sign-up form tests.

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,38 +1,105 @@
 const { Section1 } = require('../objects/section-1')
+const faker = require('faker')
 
 before(() => {
   Section1.actions.visit()
 })
 
-describe('Validate the Users table.', () => {  
+describe('Validate the Users table', () => {
   context('user table visibility', () => {
     it('is hidden when page loads', () => {
-      Section1.actions.assertUsersTableIsHidden()  
+      Section1.actions.users.assertTableIsHidden()
     })
 
     it('is visible when clicking on show button', () => {
-      Section1.actions.clickShowTable()
-      Section1.actions.assertUsersTableIsVisible()
+      Section1.actions.users.clickShowTable()
+      Section1.actions.users.assertTableIsVisible()
     })
   })
 
   context('user table structure', () => {
     it('is 5 columns wide', () => {
-      Section1.actions.assertUsersTableColumnCount(5)
+      Section1.actions.users.assertTableColumnCount(5)
     })
 
     it('is 10 rows long', () => {
-      Section1.actions.assertUsersTableRowCount(10)
+      Section1.actions.users.assertTableRowCount(10)
     })
   })
 
   context('user table data', () => {
     it('has at least 5 entries have the role "user"', () => {
-      Section1.actions.assertUserCountWithRole('user', 5, false)
+      Section1.actions.users.assertUserCountWithRole('user', 5, false)
     })
 
     it('has exactly 3 people older than 60 years old', () => {
-      Section1.actions.assertUserCountOlderThanAge(60, 3)
+      Section1.actions.users.assertUserCountOlderThanAge(60, 3)
     })
   })
+})
+
+describe('Validate the Sign-Up Form', () => {
+
+  context('sign-up form visibility', () => {
+
+    it('is hidden when page loads', () => {
+      Section1.actions.signUpForm.assertFormIsHidden()
+    })
+
+    it('is visible after clicking on "Show Form" button.', () => {
+      Section1.actions.signUpForm.clickShowForm()
+      Section1.actions.signUpForm.assertFormIsVisible()
+    })
+
+  })
+
+  context('sign-up form structure', () => {
+    it('has the "Name" field and it is a text input')
+
+    it('has the "Age" field and it is a number input')
+
+    it('has the "Gender" field and it is a select')
+
+    it('has the "Nurse?" field and it is a check box input')
+
+    it('has the "Submit" button')
+  })
+
+  context('fill up the sign-up form', () => {
+
+    it('the "Name" field can be filled', () => {
+      console.log(faker)
+      const name = `${faker.name.firstName()} ${faker.name.lastName()}`
+      Section1.actions.signUpForm.setName(name)
+      Section1.actions.signUpForm.assertNameValue(name)
+    })
+
+    it('the "Age" field can be filled', () => {
+      const age = 21 + Math.floor(Math.random() * 100)
+      Section1.actions.signUpForm.setAge(age)
+      Section1.actions.signUpForm.assertAgeValue(age)
+    })
+
+    it('the "Gender" field can be selected', () => {
+      const gender = 'Female'
+      const value = Section1.literals.SIGN_UP_FORM_GENDER_VALUES.FEMALE
+      Section1.actions.signUpForm.selectGender(value)
+      Section1.actions.signUpForm.assertGenderValue(gender, value)
+    })
+
+    it('the "Nurse?" field can be set', () => {
+      Section1.actions.signUpForm.setNurse(true)
+      Section1.actions.signUpForm.assertNurseValue(true)
+    })
+
+  })
+
+  context('form submission', () => {
+    it('the message "Form Submitted!" is displayed after submission', () => {
+      Section1.actions.signUpForm.stubFormSubmittedAlertAssert()
+      Section1.actions.signUpForm.clickSubmit()
+      Section1.actions.signUpForm.clickOkInAlert()
+    })
+  })
+
 })
--- cypress/objects/section-1.js
+++ cypress/objects/section-1.js
@@ -6,11 +6,18 @@ const Section1 = {
     PAGE_URL: '/section-1',
     HEADER_TEXT: 'DOM interactions',
     USER_TABLE_COLUMN_MAP: {
-      "id": 1,
-      "first_name": 2,
-      "last_name": 3,
-      "dob": 4,
-      "role": 5
+      ID: 1,
+      FIRST_NAME: 2,
+      LAST_NAME: 3,
+      DOB: 4,
+      ROLE: 5
+    },
+    SIGN_UP_FORM_GENDER_VALUES: {
+      MALE: 'male',
+      FEMALE: 'female',
+      NON_BINARY: 'nbin',
+      OTHER: 'other',
+      PREFER_NOT_TO_SAY: 'prefns'
     }
   },
 
@@ -19,10 +26,21 @@ const Section1 = {
    */
   elements: {
     hdrSection1: 'h1:first',
-    tblUsers: '[data-test=user-table]',
-    tblUsersHeader: '[data-test=table-header]',
-    tblUsersRows: 'tr:not(:first)',
-    btnShowTable: '[data-test=table-toggle-button]'
+    users: {
+      tblUsers: '[data-test=user-table]',
+      tblUsersHeader: '[data-test=table-header]',
+      tblUsersRows: 'tr:not(:first)',
+      btnShowTable: '[data-test=table-toggle-button]'
+    },
+    signUpForm: {
+      form: '[data-test=signup-form]',
+      txtFullname: '[data-test=full-name-input]',
+      txtAge: '[data-test=age-input]',
+      dpwGender: '[data-test=gender-select]',
+      chkNurse: '[data-test=nurse-input]',
+      btnSubmit: '[data-test=submit-btn]',
+      btnShowForm: '[data-test=form-toggle-button]'
+    }
   },
 
   /**
@@ -40,112 +58,247 @@ const Section1 = {
         .should('have.text', Section1.literals.HEADER_TEXT)
     },
 
-    /**
+    users: {
+      /**
      * Clicks on the button "Show Table".
      */
-    clickShowTable() {
-      cy.get(Section1.elements.btnShowTable).click()
-    },
+      clickShowTable() {
+        cy.get(Section1.elements.users.btnShowTable).click()
+      },
 
-    /**
-     * Asserts that the users table is hidden.
-     */
-    assertUsersTableIsHidden() {
-      cy.get(Section1.elements.tblUsers)
-        .should('be.hidden')
-    },
-
-    /**
-     * Asserts that the users table is visible.
-     */
-    assertUsersTableIsVisible() {
-      cy.get(Section1.elements.tblUsers)
-        .should('be.visible')
-    },
-
-    /**
-     * Finds the header of the users table and asserts the amount of columns
-     * matches the expected.
-     * @param {Number} expectedCount the expected count of columns.
-     */
-    assertUsersTableColumnCount(expectedCount) {
-      cy.get(Section1.elements.tblUsers)
-        .find(Section1.elements.tblUsersHeader)
-        .children()
-        .should('have.length', expectedCount)
-    },
-
-    /**
-     * Finds the body of the users table and asserts the amount of rows
-     * excluding the header matches the expected.
-     * @param {Number} expectedCount the expected count of rows
-     */
-    assertUsersTableRowCount(expectedCount) {
-      cy.get(Section1.elements.tblUsers)
-        .find(Section1.elements.tblUsersRows)
-        .should('have.length', expectedCount)
-    },
-
-    /**
-     * Finds the list of users in the user table and asserts that a given count
-     * of them has an expected role.
-     * @param {String} role the role to be searched for
-     * @param {Number} expectedCount the expected count of users to have the
-     *                 expected role.
-     * @param {Boolean} exact will assert for the exact count if true, else
-     *                  asserts for the count to be greater or equal the given
-     *                  expected user count.
-     */
-    assertUserCountWithRole(role, expectedCount, exact = true) {
-      let condition = (exact? 'eq' : 'be.gt')
-      cy.get(Section1.elements.tblUsers)
-        .find(Section1.elements.tblUsersRows)
-        .find(`th:nth-child(${Section1.literals.USER_TABLE_COLUMN_MAP.role})`)
-        .filter(`:contains("${role}")`)
-        .its('length')
-        .should(condition, expectedCount)
-    },
-
-    /**
-     * Finds the list of users in the user table and asserts that a given count
-     * of them is older then a given age.
-     * @param {Number} age the age to be searched forsss
-     * @param {Number} expectedCount the expected count of users to be older 
-     *                 than the age provided.
-     * @param {Boolean} exact will assert for the exact count if true, else
-     *                  asserts for the count to be greater or equal the given
-     *                  expected user count.
-     */
-    assertUserCountOlderThanAge(age, expectedCount, exact = true) {
-      
       /**
-       * Splits a date string and returns the year portion of it. Assumes the
-       * format of the string is %m/%d/%Y.
-       * @param {String} value the date in string format.
-       * @returns the year represented in the date string
+       * Asserts that the users table is hidden.
        */
-      let dobYear = function(value) {
-        return parseInt(value.split('/')[2])
-      }
-      
+      assertTableIsHidden() {
+        cy.get(Section1.elements.users.tblUsers)
+          .should('be.hidden')
+      },
+
       /**
-       * Parses the DOB from a given cell of the table and returns if the age
-       * of the user is older than a given age.
-       * @param {Number} index index of the element in the jquery array.
-       * @param {Object} column the element containing the date of birth.
+       * Asserts that the users table is visible.
        */
-      let isOlderThanAge = function (_, column) {
-        return new Date().getFullYear() - dobYear(column.innerText) > age
+      assertTableIsVisible() {
+        cy.get(Section1.elements.users.tblUsers)
+          .should('be.visible')
+      },
+
+      /**
+       * Finds the header of the users table and asserts the amount of columns
+       * matches the expected.
+       * @param {Number} expectedCount the expected count of columns.
+       */
+      assertTableColumnCount(expectedCount) {
+        cy.get(Section1.elements.users.tblUsers)
+          .find(Section1.elements.users.tblUsersHeader)
+          .children()
+          .should('have.length', expectedCount)
+      },
+
+      /**
+       * Finds the body of the users table and asserts the amount of rows
+       * excluding the header matches the expected.
+       * @param {Number} expectedCount the expected count of rows
+       */
+      assertTableRowCount(expectedCount) {
+        cy.get(Section1.elements.users.tblUsers)
+          .find(Section1.elements.users.tblUsersRows)
+          .should('have.length', expectedCount)
+      },
+
+      /**
+       * Finds the list of users in the user table and asserts that a given count
+       * of them has an expected role.
+       * @param {String} role the role to be searched for
+       * @param {Number} expectedCount the expected count of users to have the
+       *                 expected role.
+       * @param {Boolean} exact will assert for the exact count if true, else
+       *                  asserts for the count to be greater or equal the given
+       *                  expected user count.
+       */
+      assertUserCountWithRole(role, expectedCount, exact = true) {
+        const condition = (exact ? 'eq' : 'be.gt')
+        cy.get(Section1.elements.users.tblUsers)
+          .find(Section1.elements.users.tblUsersRows)
+          .find(`th:nth-child(${Section1.literals.USER_TABLE_COLUMN_MAP.ROLE})`)
+          .filter(`:contains("${role}")`)
+          .its('length')
+          .should(condition, expectedCount)
+      },
+
+      /**
+       * Finds the list of users in the user table and asserts that a given count
+       * of them is older then a given age.
+       * @param {Number} age the age to be searched forsss
+       * @param {Number} expectedCount the expected count of users to be older 
+       *                 than the age provided.
+       * @param {Boolean} exact will assert for the exact count if true, else
+       *                  asserts for the count to be greater or equal the given
+       *                  expected user count.
+       */
+      assertUserCountOlderThanAge(age, expectedCount, exact = true) {
+
+        /**
+         * Splits a date string and returns the year portion of it. Assumes the
+         * format of the string is %m/%d/%Y.
+         * @param {String} value the date in string format.
+         * @returns the year represented in the date string
+         */
+        const dobYear = function (value) {
+          return parseInt(value.split('/')[2])
+        }
+
+        /**
+         * Parses the DOB from a given cell of the table and returns if the age
+         * of the user is older than a given age.
+         * @param {Number} index index of the element in the jquery array.
+         * @param {Object} column the element containing the date of birth.
+         */
+        const isOlderThanAge = function (_, column) {
+          return new Date().getFullYear() - dobYear(column.innerText) > age
+        }
+
+        const condition = (exact ? 'eq' : 'be.gt')
+        cy.get(Section1.elements.users.tblUsersRows)
+          .find(`th:nth-child(${Section1.literals.USER_TABLE_COLUMN_MAP.DOB})`)
+          .filter((index, column) => { return isOlderThanAge(index, column) })
+          .its('length')
+          .should(condition, expectedCount)
+      }
+    },
+
+    signUpForm: {
+
+      /**
+       * Asserts that the Sign-Up form is hidden.
+       */
+      assertFormIsHidden() {
+        cy.get(Section1.elements.signUpForm.form)
+          .should('be.hidden')
+      },
+
+      /**
+       * Asserts that the Sign-Up form is visible.
+       */
+      assertFormIsVisible() {
+        cy.get(Section1.elements.signUpForm.form)
+          .should('be.visible')
+      },
+
+      /**
+       * Asserts the value from the Name field.
+       * @param {String} expected the value expected to be in the name field.
+       */
+      assertNameValue(expected) {
+        cy.get(Section1.elements.signUpForm.txtFullname)
+          .should('have.value', expected)
+      },
+
+      /**
+       * Asserts the value from the Age field.
+       * @param {String} expected the value expected to be in the age field.
+       */
+      assertAgeValue(expected) {
+        cy.get(Section1.elements.signUpForm.txtAge)
+          .should('have.value', expected)
+      },
+
+      /**
+       * Asserts the value and the visible text from the Gender field.
+       * @param {String} text the text of the option expected to be selected.
+       * @param {String} value the value of the option expected to be selected.
+       */
+      assertGenderValue(text, value) {
+        cy.get(Section1.elements.signUpForm.dpwGender)
+          .find('option:selected')
+          .should('have.value', value)
+          .should('have.text', text)
+      },
+
+      /**
+       * Asserts the value of the Nurse field.
+       * @param {Boolean} expected wheter the field should be checked or not.
+       */
+      assertNurseValue(expected) {
+        const assert = (expected ? 'be.checked' : 'not.to.be.checked')
+        cy.get(Section1.elements.signUpForm.chkNurse)
+          .should(assert)
+      },
+
+      /**
+       * Sets up the stub function for asserting the text of the "Form
+       * submitted" pop-up alert.
+       */
+      stubFormSubmittedAlertAssert() {
+        cy.on('window:alert', (text) => {
+          expect(text).to.equal('Form submitted!')
+        })
+      },
+
+      /**
+       * Sets the value of the "Name" field.
+       * @param {String} value the name to be input in the Name field.
+       */
+      setName(value) {
+        cy.get(Section1.elements.signUpForm.txtFullname)
+          .type(value)
+      },
+
+      /**
+       * Sets the value fo the "Age"field.
+       * @param {Number} value the age to be input in the Age field.
+       */
+      setAge(value) {
+        cy.get(Section1.elements.signUpForm.txtAge)
+          .type(value)
+      },
+
+      /**
+       * Selects an option in the Gender select field based on the value of
+       * the option (not the visible text).
+       * @param {String} value the value of the gender option to be selected.
+       */
+      selectGender(value) {
+        cy.get(Section1.elements.signUpForm.dpwGender)
+          .select(value)
+      },
+
+      /**
+       * Checks or unchecks the Nurse? checkbox.
+       * @param {Boolean} value wheter the checkbox has to be checked or not.
+       */
+      setNurse(value) {
+        const checkbox = cy.get(Section1.elements.signUpForm.chkNurse)
+        if (value) {
+          checkbox.check()
+          return
+        }
+        checkbox.uncheck()
+      },
+
+      /**
+       * Clicks on the Submit button.
+       */
+      clickSubmit() {
+        cy.get(Section1.elements.signUpForm.btnSubmit).click()
+      },
+
+      /**
+       * Clicks on the Show Form button.
+       */
+      clickShowForm() {
+        cy.get(Section1.elements.signUpForm.btnShowForm).click()
+      },
+
+      /**
+       * Confirms the "Form Submitted" alert.
+       */
+      clickOkInAlert() {
+        cy.on('window:confirm', () => true)
       }
 
-      let condition = (exact? 'eq' : 'be.gt')
-      cy.get(Section1.elements.tblUsersRows)
-      .find(`th:nth-child(${Section1.literals.USER_TABLE_COLUMN_MAP.dob})`)
-      .filter((index, column) => {return isOlderThanAge(index, column)})
-      .its('length')
-      .should(condition, expectedCount)
     }
-  },
+
+  }
 }
 
 module.exports = { Section1 }
